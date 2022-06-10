053744509d032ae7ceec7ee1df12c4d54408ddc9
Senryoku <maretverdant@gmail.com>
2022-06-10 15:00:52 +0200

    Some Tokenizer tests

---
+++ test/tokenizer.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+
+#include <string>
+#include <vector>
+
+#include <Logger.hpp>
+#include <Tokenizer.hpp>
+
+namespace TokenizerTests {
+
+#define TOKENIZE(code)                               \
+    std::string                   source{code};      \
+    std::vector<Tokenizer::Token> tokens;            \
+    Tokenizer                     tokenizer(source); \
+    while(tokenizer.has_more())                      \
+        tokens.push_back(tokenizer.consume());       \
+    EXPECT_GT(tokens.size(), 0);
+
+TEST(Tokenizer, Add) {
+    TOKENIZE("25 + 97;");
+    EXPECT_EQ(tokens.size(), 4);
+    EXPECT_EQ(tokens[0].type, Tokenizer::Token::Type::Digits);
+    EXPECT_EQ(tokens[1].type, Tokenizer::Token::Type::Operator);
+    EXPECT_EQ(tokens[2].type, Tokenizer::Token::Type::Digits);
+    EXPECT_EQ(tokens[3].type, Tokenizer::Token::Type::Control);
+}
+
+TEST(Tokenizer, Assignment) {
+    TOKENIZE("int i = 0;");
+    EXPECT_EQ(tokens.size(), 5);
+    EXPECT_EQ(tokens[0].type, Tokenizer::Token::Type::BuiltInType);
+    EXPECT_EQ(tokens[1].type, Tokenizer::Token::Type::Identifier);
+    EXPECT_EQ(tokens[2].type, Tokenizer::Token::Type::Operator);
+    EXPECT_EQ(tokens[3].type, Tokenizer::Token::Type::Digits);
+    EXPECT_EQ(tokens[4].type, Tokenizer::Token::Type::Control);
+}
+
+} // namespace TokenizerTests
