84097321c407b7cd431154327334998866a36bbb
Senryoku <maretverdant@gmail.com>
2022-06-16 09:33:12 +0200

    Improved tokenizer error messages.

--- src/core/Tokenizer.cpp
+++ src/core/Tokenizer.cpp
@@ -1,4 +1,4 @@
-#include "Tokenizer.hpp"
+﻿#include "Tokenizer.hpp"
 
 void Tokenizer::advance() noexcept {
     if(is_newline(peek()))
@@ -30,12 +30,12 @@ Tokenizer::Token Tokenizer::search_next() {
             case '\'': {
                 type = Token::Type::CharLiteral;
                 if(eof())
-                    throw Exception(fmt::format("[Tokenizer] Reached end of file without matching ' on line {}.", _current_line),
+                    throw Exception(fmt::format("[Tokenizer] Error: Reached end of file without matching ' on line {}.", _current_line),
                                     point_error(begin, _current_line, begin, _source.length() - 1));
                 if(peek() == '\\') { // Escaped character
                     advance();
                     if(eof())
-                        throw Exception(fmt::format("[Tokenizer] Expected escape sequence, got EOF on line {}.", _current_line),
+                        throw Exception(fmt::format("[Tokenizer] Error: Expected escape sequence, got EOF on line {}.", _current_line),
                                         point_error(begin, _current_line, begin, _source.length() - 1));
                     size_t c = 0;
                     switch(peek()) {
@@ -50,18 +50,18 @@ Tokenizer::Token Tokenizer::search_next() {
                         case 'r': c = 8; break;
                         case 't': c = 9; break;
                         case 'v': c = 10; break;
-                        default: throw Exception(fmt::format("[Tokenizer] Unknown escape sequence \\'{}'.", peek()), point_error(_current_pos, _current_line, begin));
+                        default: throw Exception(fmt::format("[Tokenizer] Error: Unknown escape sequence \\'{}'.", peek()), point_error(_current_pos, _current_line, begin));
                     }
                     advance();
                     if(eof() || peek() != '\'')
-                        throw Exception(fmt::format("[Tokenizer] Reached end of file without matching ' on line {}.", _current_line),
+                        throw Exception(fmt::format("[Tokenizer] Error: Reached end of file without matching ' on line {}.", _current_line),
                                         point_error(begin, _current_line, begin, _source.length() - 1));
                     advance(); // Skip '
                     return Token{type, std::string_view{escaped_char + c, escaped_char + c + 1}, _current_line, _current_column};
                 } else {
                     advance();
                     if(eof() || peek() != '\'')
-                        throw Exception(fmt::format("[Tokenizer] Reached end of file without matching ' on line {}.", _current_line),
+                        throw Exception(fmt::format("[Tokenizer] Error: Reached end of file without matching ' on line {}.", _current_line),
                                         point_error(begin, _current_line, begin, _source.length() - 1));
                     advance(); // Skip '
                     return Token{type, std::string_view{_source.begin() + begin + 1, _source.begin() + (_current_pos - 1)}, _current_line, _current_column};
@@ -72,7 +72,7 @@ Tokenizer::Token Tokenizer::search_next() {
                 while(!eof() && peek() != '"') // TODO: Handle escaped "
                     advance();
                 if(eof())
-                    throw Exception(fmt::format("[Tokenizer] Reached end of file without matching \" on line {}.", _current_line),
+                    throw Exception(fmt::format("[Tokenizer] Error: Reached end of file without matching \" on line {}.", _current_line),
                                     point_error(begin, _current_line, begin, _source.length() - 1));
                 advance(); // Skip '"'
                 type = Token::Type::StringLiteral;
@@ -100,7 +100,8 @@ Tokenizer::Token Tokenizer::search_next() {
                     while(!eof() && (is_digit(peek()) || peek() == '.')) {
                         if(peek() == '.') {
                             if(found_decimal_separator)
-                                throw Exception(fmt::format("Usupernumerary '.' in float constant on line {}.", _current_line), point_error(_current_pos, _current_line, begin));
+                                throw Exception(fmt::format("[Tokenizer] Error: Unexpected supernumerary '.' in float constant on line {}.", _current_line),
+                                                point_error(_current_pos, _current_line, begin));
                             found_decimal_separator = true;
                         }
                         advance();
@@ -116,7 +117,7 @@ Tokenizer::Token Tokenizer::search_next() {
                     while(temp_cursor != begin && !operators.contains(std::string_view{_source.begin() + begin, _source.begin() + temp_cursor}))
                         --temp_cursor;
                     if(temp_cursor == begin)
-                        throw Exception(fmt::format("Error: No matching operator for '{}'.", std::string_view{_source.begin() + begin, _source.begin() + end}),
+                        throw Exception(fmt::format("[Tokenizer] Error: No matching operator for '{}'.", std::string_view{_source.begin() + begin, _source.begin() + end}),
                                         point_error(temp_cursor, _current_line, begin, end));
                     // Sync our cursor with the temp one.
                     while(_current_pos != temp_cursor)
@@ -138,33 +139,36 @@ Tokenizer::Token Tokenizer::search_next() {
 }
 
 std::string Tokenizer::point_error(size_t at, size_t line, int from, int to) const noexcept {
+    assert((from == -1 || to == -1) || from <= to);
     std::string return_value = "";
     // Search previous line break
     auto line_start = at;
     while(line_start > 0 && _source[line_start] != '\n')
         --line_start;
-    ++line_start;
+    if(_source[line_start] == '\n')
+        ++line_start;
     auto line_end = at;
     while(line_end < _source.size() && _source[line_end] != '\n')
         ++line_end;
-    auto line_info = fmt::format("Line {}: ", line); // FIXME:
+    auto line_info = fmt::format("{: >5} | ", line);
+    auto padding = fmt::format("{: >{}} | ", "", line_info.size() - 3);
     // Display the source line containing the error
     return_value += fmt::format("{}{}\n", line_info, std::string_view{_source.begin() + line_start, _source.begin() + line_end});
     // Point at it
     if(from < 0 && to < 0) {
-        return_value += fmt::format("{:>{}s}\n", "^", line_info.size() + at - line_start + 1);
+        return_value += fmt::format("{}{:>{}s}\n", padding, "^", at - line_start + 1);
     } else {
         if(from < 0)
             from = static_cast<int>(at);
         if(to < 0)
             to = static_cast<int>(at);
         std::string str = "";
-        for(size_t i = 0; i < std::max(at, static_cast<size_t>(std::max(to, from))) - line_start + line_info.size() + 1; ++i)
+        for(size_t i = 0; i < std::max(at, static_cast<size_t>(std::max(to, from))) - line_start + 1; ++i)
             str += " ";
-        for(size_t i = from - line_start + line_info.size(); i < to - line_start + line_info.size(); ++i)
+        for(size_t i = from - line_start; i < std::min(to - line_start, str.size()); ++i)
             str[i] = '~';
-        str[at - line_start + line_info.size()] = '^';
-        return_value += fmt::format("{}\n", str);
+        str[at - line_start] = '^';
+        return_value += fmt::format("{}{}\n", padding, str);
     }
     return return_value;
 }
