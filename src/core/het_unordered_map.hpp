46b967413886ac4382ee3097f4cb6e83999e923a
Senryoku <maretverdant@gmail.com>
2022-09-08 06:41:10 +0200

    Try to fix compilation on clang

--- src/core/het_unordered_map.hpp
+++ src/core/het_unordered_map.hpp
@@ -13,7 +13,7 @@ struct string_hash {
     using is_transparent = void;
     using key_equal = std::equal_to<>;             // Pred to use
     using hash_type = std::hash<std::string_view>; // just a helper local type
-    size_t operator()(std::string_view txt) const { return hash_type{}(txt); }
+    size_t operator()(const std::string_view txt) const { return hash_type{}(txt); }
     size_t operator()(const std::string& txt) const { return hash_type{}(txt); }
     size_t operator()(const char* txt) const { return hash_type{}(txt); }
 };
