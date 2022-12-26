1ef902305ffb4bf2aa520acc281f0ddd28ef94e4
Senryoku <maretverdant@gmail.com>
2022-12-27 00:12:12 +0100

    TODOs and DEBUG :)

--- stdlib/memory.cpp
+++ stdlib/memory.cpp
@@ -1,12 +1,22 @@
 #include <cstdlib>
 
+// DEBUG
+#include <cstdint>
+#include <cstdio>
+
 extern "C" {
 void* __malloc(size_t size) {
-    return malloc(size);
+    auto ptr = malloc(size);
+    printf("\t<std/memory> malloc(%llu) -> 0x%016x\n", size, ptr);
+    return ptr;
 }
 
 void __free(void* ptr) {
+    printf("\t<std/memory> free(0x%016x)\n", ptr);
+    printf("\t<std/memory>   0: %llu\n", *static_cast<uint64_t*>(ptr));
+    printf("\t<std/memory>   1: %llu\n", *(static_cast<uint64_t*>(ptr) + 1));
     free(ptr);
+    printf("\t<std/memory> -> free(0x%016x) done\n", ptr);
 }
 
 void* null_pointer() {
