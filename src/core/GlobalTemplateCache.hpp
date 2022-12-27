715ee66df7e86b9667676b56fd24143ce1096c87
Senryoku <maretverdant@gmail.com>
2022-12-28 00:56:37 +0100

    Notes

--- src/core/GlobalTemplateCache.hpp
+++ src/core/GlobalTemplateCache.hpp
@@ -5,6 +5,9 @@
 
 #include <AST.hpp>
 
+/* NOTE/FIXME: As the name suggests, this is not scoped.
+ *             Nothing prevents name clashing between files, this may come to bite use later :))
+ */
 class GlobalTemplateCache {
   public:
     inline static GlobalTemplateCache& instance() {
@@ -15,8 +18,16 @@ class GlobalTemplateCache {
     const AST::FunctionDeclaration* get_function(const std::string& name);
     const AST::TypeDeclaration* get_type(const std::string& name);
 
-    inline void register_function(const AST::FunctionDeclaration& node) { _functions.emplace(std::string(node.token.value), node.clone()); }
-    inline void register_type(const AST::TypeDeclaration& node) { _types.emplace(std::string(node.token.value), node.clone()); }
+    inline void register_function(const AST::FunctionDeclaration& node) { 
+        if(_functions.contains(std::string(node.token.value)))
+            warn("[GlobalTemplateCache::register_function] Templated function '{}' already registered.\n", node.token.value);
+        _functions.emplace(std::string(node.token.value), node.clone()); 
+    }
+    inline void register_type(const AST::TypeDeclaration& node) {
+        if(_types.contains(std::string(node.token.value)))
+            warn("[GlobalTemplateCache::register_type] Templated type '{}' already registered.\n", node.token.value);
+        _types.emplace(std::string(node.token.value), node.clone());
+    }
 
   private:
     GlobalTemplateCache() =default;
